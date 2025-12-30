6a9b6f97fe37e944ed0e480464a4edf1a0775f50
kavinrajan <kavinece53@gmail.com>
2025-12-30 21:11:54 +0530

    Fast API todo task  Initial commit

---
+++ database.py
@@ -0,0 +1,36 @@
+import os;
+from sqlalchemy import create_engine;
+from sqlalchemy.orm import sessionmaker, declarative_base;
+from dotenv import load_dotenv;
+
+load_dotenv()
+
+# ALWAYS use string key in getenv
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Safe local fallback (SQLite)
+# if not DATABASE_URL:
+#    DATABASE_URL = "sqlite:///./todo.db"
+
+# engine = create_engine(
+#     DATABASE_URL,
+#     connect_args={"check_same_thread": False}
+#     if DATABASE_URL.startswith("sqlite")
+#     else {}
+# )
+
+#engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
